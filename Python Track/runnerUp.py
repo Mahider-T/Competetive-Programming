42bce935a959f7d62cdc9249c8a0d0b0375e9f57
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-16 06:15:15 +0300

    Identify the runner up from a list

--- Python Track/runnerUp.py
+++ Python Track/runnerUp.py
@@ -1,14 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
-    # top = arr[0]
-    # runnerUp = arr[0]
-    # for x in arr:
-    #      if x > top :
-    #         runnerUp = top
-    #         top = x
-    #      elif x > runnerUp and x != runnerUp:
-    #          runnerUp = x
     maximum = max(arr)
     while(max(arr) == maximum):
         arr.remove(max(arr))
