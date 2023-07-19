da5042e66672c818f5f8863b62968fa657ced893
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-07-19 15:16:55 +0300

    Final lists

--- Python Track/lists.py
+++ Python Track/lists.py
@@ -1,10 +1,34 @@
 if __name__ == '__main__':
     N = int(input())
     operations = ["insert", "print", "remove", "append", "sort", "pop", "reverse"]
+    numbers = []
     for i in range (N):
         choice = input().split()
         if choice[0] not in operations:
             print("No such operations. Choose among", end = " ")
             print(operations)
+        elif choice[0] == "insert":
+            # print(choice[1], end = " ")
+            # print( choice[2])
+            position = int(choice[1])
+            element = int(choice[2])
+            numbers.insert(position, element)
+            # print(numbers)
+        elif choice[0] == "print":
+            print(numbers)
+        elif choice[0] == "remove":
+            numbers.remove(int(choice[1]))
+            # print(numbers)
+        elif choice[0] == "append":
+            numbers.append(int(choice[1]))
+            # print(numbers)
+        elif choice[0] == "sort":
+            numbers.sort()
+            # print(numbers)
+        elif choice[0] == "pop":
+            numbers.pop(-1)
+            # print(numbers)
         else:
-            print("Successfully received!")
+            numbers.reverse()
+            # print(numbers)
+# print(numbers)
