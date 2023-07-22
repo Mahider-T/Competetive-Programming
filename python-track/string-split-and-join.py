718ac36733099410ecf815dc7fade3e3d26f23c3
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-07-22 20:41:25 +0300

    String Split and Join

---
+++ python-track/string-split-and-join.py
@@ -0,0 +1,10 @@
+def split_and_join(line):
+    # write your code here
+    result = line.split(" ")
+    result = "-".join(result)
+    return result
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
